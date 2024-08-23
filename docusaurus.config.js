ec31c55d7581d40af5029f70b65d0754a8fe97cb
JoseRamonMas <joseramonmas.informatica@iespacomolla.es>
2024-08-23 11:44:44 +0200

    canvis titol

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,8 +8,8 @@ import {themes as prismThemes} from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'Documents de Programació',
+  tagline: 'Programació en 1r de DAW',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
